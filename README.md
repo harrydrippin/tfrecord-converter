a3f0f16dfd4efa65aedc0ed63b5449a8d2ff5b49
Seunghwan Hong <seunghwan@scatterlab.co.kr>
2020-04-27 12:14:13 +0900

    Initial commit
    Signed-off-by: Seunghwan Hong <seunghwan@scatterlab.co.kr>

---
+++ README.md
@@ -0,0 +1,6 @@
+# TFRecord Converter
+
+## Key Feature
+
+- [ ] Generate TFRecord files by given CSV, TSV files automatically
+- [ ] Upload to Google Cloud Storage for convenient usage while using TPU with TensorFlow
---
+++ setup.py
@@ -0,0 +1,12 @@
+  from setuptools import find_packages, setup
+
+setup(
+    name="tfrecord-converter",
+    version="0.0.1",
+    description="Covert CSV, TSV files to TFRecord and upload to Google Cloud Storage automatically",
+    install_requires=[],
+    url="https://github.com/harrydrippin/tfrecord-converter.git",
+    author="Seunghwan Hong",
+    author_email="harrydrippin@gmail.com",
+    packages=find_packages(exclude=["tests"]),
+)
