3816568d30c1761660ed9f03162bff04235dbb20
Seunghwan Hong <seunghwan@scatterlab.co.kr>
2020-04-27 17:12:14 +0900

    feat: Add fileio.py and tests

---
+++ tests/test_fileio.py
@@ -0,0 +1,37 @@
+import pytest
+
+from tfrecord_converter.fileio import get_filenames, read_file
+
+
+@pytest.fixture
+def expected_file_output():
+    return [
+        ["SEND", "20200427030101", "1", "Hello! Nice to meet you :)"],
+        ["RECV", "20200427030202", "1", "Good to see you!"],
+    ]
+
+
+def test_get_filenames():
+    expected = set(
+        [
+            "./tests/data/sample_csv.csv",
+            "./tests/data/sample_csv_with_header.csv",
+            "./tests/data/sample_tsv.tsv",
+            "./tests/data/sample_tsv_with_header.tsv",
+        ]
+    )
+    assert expected == set(get_filenames("./tests/data/*.*"))
+
+
+@pytest.mark.parametrize(
+    "path, mode, skip_header",
+    [
+        pytest.param("./tests/data/sample_csv.csv", "csv", False),
+        pytest.param("./tests/data/sample_csv_with_header.csv", "csv", True),
+        pytest.param("./tests/data/sample_tsv.tsv", "tsv", False),
+        pytest.param("./tests/data/sample_tsv_with_header.tsv", "tsv", True),
+    ],
+    ids=["CSV", "CSV (Header)", "TSV", "TSV (Header)"],
+)
+def test_read_file(path, mode, skip_header, expected_file_output):
+    assert expected_file_output == read_file(path, mode, skip_header=skip_header)
---
+++ tfrecord_converter/fileio.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from typing import List
+import glob
+
+
+def get_filenames(glob_string: str) -> List[str]:
+    """Load every filenames matches with given glob string."""
+    return glob.glob(glob_string, recursive=True)
+
+
+def read_file(path: str, mode: str, skip_header: bool = False) -> List[List[str]]:
+    """
+    Read the file by given mode and path.
+
+    :param path: File path.
+    :param mode: File parsing mode. e.g. csv, tsv
+    :param skip_header: Whether skip the header or not
+    """
+    mode = mode.lower()
+    if mode not in ("csv", "tsv"):
+        raise ValueError(f"File mode should be 'csv' or 'tsv', not {mode}")
+    with open(path, "r") as f:
+        raw = f.read().split("\n")
+        if skip_header:
+            raw = raw[1:]
+        if raw[-1] == "":
+            raw = raw[:-1]
+
+    delimiter = "," if mode == "csv" else "\t"
+    return [line.split(delimiter) for line in raw]
+
+
+def save_tfrecord_file(examples: List[tf.train.Example], filename: str, compression_type: str = "GZIP"):
+    """Save given examples to TFRecord file."""
+    if compression_type not in ("GZIP", "ZLIB", ""):
+        raise ValueError(f"Invalid compression type `{compression_type}` is present.")
+
+    options = tf.io.TFRecordOptions(compression_type=compression_type)
+    with tf.io.TFRecordWriter(filename, options) as writer:
+        for example in examples:
+            writer.write(example)
